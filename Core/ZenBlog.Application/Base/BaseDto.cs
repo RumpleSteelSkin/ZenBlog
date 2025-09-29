41645afe546e0d2f426baccb9b28230fa3a4a85c
RumpleSteelSkin <osmnistbayrak@gmail.com>
2025-09-29 20:04:34 +0300

    Feat(Application/Base): Created Base DTOs and Result Types.
    Introduced BaseDto in the Application layer to serve as the counterpart for the BaseEntity.
    
    Implemented BaseResult to provide structured output and status feedback for various operations and processes.

---
+++ Core/ZenBlog.Application/Base/BaseDto.cs
@@ -0,0 +1,8 @@
+namespace ZenBlog.Application.Base;
+
+public class BaseDto
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ Core/ZenBlog.Application/Base/BaseResult.cs
@@ -0,0 +1,69 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+
+namespace ZenBlog.Application.Base;
+
+public class BaseResult<T>
+{
+    public T? Data { get; set; }
+    private IEnumerable<Error>? Errors { get; set; }
+
+    [JsonIgnore] private bool IsSuccess => Errors == null || !Errors.Any();
+    [JsonIgnore] public bool IsFailure => !IsSuccess;
+
+    public static BaseResult<T> Success(T data)
+    {
+        return new BaseResult<T>
+        {
+            Data = data
+        };
+    }
+
+    public static BaseResult<T> Fail()
+    {
+        return new BaseResult<T>
+        {
+            Errors = [new Error { ErrorMessage = "There was an error processing your request." }]
+        };
+    }
+
+    public static BaseResult<T> Fail(string error)
+    {
+        return new BaseResult<T>
+        {
+            Errors = [new Error { ErrorMessage = error }]
+        };
+    }
+
+    public static BaseResult<T> Fail(IEnumerable<IdentityError> errors)
+    {
+        return new BaseResult<T>
+        {
+            Errors = from error in errors
+                select new Error { PropertyName = error.Code, ErrorMessage = error.Description }
+        };
+    }
+
+    public static BaseResult<T> Fail(IEnumerable<string> errors)
+    {
+        return new BaseResult<T>
+        {
+            Errors = from error in errors
+                select new Error { ErrorMessage = error }
+        };
+    }
+
+    public static BaseResult<T> NotFound(string message)
+    {
+        return new BaseResult<T>
+        {
+            Errors = [new Error { ErrorMessage = message }]
+        };
+    }
+}
+
+public class Error
+{
+    public string? PropertyName { get; set; }
+    public string? ErrorMessage { get; set; }
+}
