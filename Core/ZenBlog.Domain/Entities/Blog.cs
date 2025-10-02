bf5fd4018f2c744dfc7eecd7f4163a0841ab285d
RumpleSteelSkin <osmnistbayrak@gmail.com>
2025-10-02 20:51:01 +0300

    Feat(Blog/DB): Added CategoryId property to the Blog entity.
    Included the CategoryId property in the Blog entity to establish the relationship with the Category.
    
    Generated and applied a new database migration to persist this schema change.

--- Core/ZenBlog.Domain/Entities/Blog.cs
+++ Core/ZenBlog.Domain/Entities/Blog.cs
@@ -9,6 +9,7 @@ public class Blog : BaseEntity
     public string? BlogImage { get; set; }
     public string? Description { get; set; }
 
+    public Guid CategoryId { get; set; }
     public virtual Category? Category { get; set; }
     public string? UserId { get; set; }
     public virtual AppUser? User { get; set; }
--- Infrastructure/ZenBlog.Persistence/Migrations/20251002173401_Blog_CategoryId_Added.Designer.cs
+++ Infrastructure/ZenBlog.Persistence/Migrations/20251002173401_Blog_CategoryId_Added.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZenBlog.Persistence.Context;
 
@@ -11,9 +12,11 @@ using ZenBlog.Persistence.Context;
 namespace ZenBlog.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251002173401_Blog_CategoryId_Added")]
+    partial class Blog_CategoryId_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
